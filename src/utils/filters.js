f8f03b4f02aff38be23d3ac98949916724e86235
Igor <romanov.fortech@gmail.com>
2023-11-07 10:13:26 +0300

    Delete: unused

--- src/utils/filters.js
+++
@@ -1,7 +0,0 @@
-export const filterBySearch = (pokemons, search) => {
-    return pokemons.filter((pokemon) => pokemon.name.toLowerCase().includes(search.toLowerCase()));
-};
-
-export const filterByTypes = (pokemons, namesSet) => {
-    return pokemons.filter((pokemon) => namesSet.has(pokemon.name))
-}
